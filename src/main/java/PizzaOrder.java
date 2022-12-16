9f94fa076ae633df34af8c1eda74abaac8519993
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-12-16 11:34:59 -0700

    added pizza url

---
+++ src/main/java/PizzaOrder.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.ArrayList;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+@WebServlet(name = "/pizza-order", urlPatterns = "/pizza-order")
+public class PizzaOrder {
+
+}
